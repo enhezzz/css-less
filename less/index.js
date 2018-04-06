0ca2d3182232a77b46b257efb8341ed9247bf332
enhezzz <13450737557@163.com>
2018-04-06 10:50:59 +0800

    重新布局结构

--- less/index.js
+++ less/index.js
@@ -1,17 +1,19 @@
 var less = require('less');
 // var origin = require('./input/variable.less')
 var fs = require('fs');
-var dirname = '语言特性/Combinatorial Explosion';
+var dirname = '函数手册/杂项函数';
 var preName = 'index';
 var origin = fs.readFileSync('./'+dirname+'/input/'+ preName+ '.less','utf8')
 // console.log(origin)
 less.render(origin,
     {
-      paths: ['.', './语言特性/Import Directives/input'],  // Specify search paths for @import directives
+      paths: ['.', './函数手册/杂项函数/input'],  // Specify search paths for @import directives
       filename: 'style.less', // Specify a filename, for better error messages
-      compress: false          // Minify CSS output
+      compress: false,          // Minify CSS output
+      sourceMap: {sourceMapFileInline: true}
     },
     function (e, output) {
-        fs.writeFileSync('./'+dirname+'/output/'+preName+'.css',output.css)
+        fs.writeFileSync('./'+dirname+'/output/'+preName+'.css',output.css);
+        // console.log(output)
        console.log(output.css);
     });
